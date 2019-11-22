bf922cbeb6823a6d985ef2ac953e847d344ee485
Lina Kovalevskaya <heike3291@gmail.com>
2019-11-23 00:40:14 +0300

    create project (#2)

---
+++ src/bookstore/bookstore.site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bookstore.site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
