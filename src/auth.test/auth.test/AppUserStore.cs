d155d13b6bf849e1e10576d09f4e43ec5a8d5c14
Lina Kovalevskaya <heike3291@gmail.com>
2019-11-23 21:23:24 +0300

    custom auth

---
+++ src/auth.test/auth.test/AppUserStore.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace auth.test
+{
+    public class AppUserStore : IUserRoleStore<AppUser>, IUserPasswordStore<AppUser>
+    {
+        public Task AddToRoleAsync(AppUser user, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> CreateAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(IdentityResult.Success);
+            // throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public Task<AppUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            //return Task.FromResult(null);
+            return Task.FromResult<AppUser>(null);// new AppUser());
+            //throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            IList<string> li = new List<string> { "Boss" };
+            return Task.FromResult(li);
+            //throw new NotImplementedException();
+        }
+
+        public Task<string> GetUserIdAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+            //throw new NotImplementedException();
+        }
+
+        public Task<string> GetUserNameAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task<IList<AppUser>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(AppUser user, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromRoleAsync(AppUser user, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedUserNameAsync(AppUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+            //throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(AppUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+            //throw new NotImplementedException();
+        }
+
+        public Task SetUserNameAsync(AppUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/auth.test/auth.test/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using auth.test.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace auth.test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private SignInManager<AppUser> _signInManager;
+        private UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            /*var a = _signInManager.IsSignedIn(User);
+
+            if (!a)
+            {
+                _signInManager.SignInAsync(new AppUser { Id = Guid.NewGuid().ToString(), UserName = "AAAA" }, false);
+            }
+
+            var b4 = _signInManager.IsSignedIn(User);*/
+
+            var x =  _userManager.CreateAsync(new AppUser { Id = Guid.NewGuid().ToString(), UserName = "uuuuuuuuuuuuu" }, "psswd12dfJdl_88");
+            var b = x.Result;
+            //_userManager.Cr
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
