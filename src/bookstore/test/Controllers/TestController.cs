2c269b68334b537ea04b2532f151a50c3f170ec5
Lina Kovalevskaya <heike3291@gmail.com>
2019-11-23 18:51:41 +0300

    try - MVC in separate modules

---
+++ src/bookstore/test/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace test.Controllers
+{
+    public class TestController: Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
